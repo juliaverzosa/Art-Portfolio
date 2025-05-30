ba91a1f85461a7f923193222ee45066e320b5501
juliaverzosa <j.verzosa.534381@umindanao.edu.ph>
2025-05-30 18:15:12 +0800

    Updates

---
+++ precache-manifest.e6e9e9aae73e88dea033681e6203f02b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c06466cc3814745c5c8bb382bd76b3",
+    "url": "/juliaverzosa/Art-Portfolio/index.html"
+  },
+  {
+    "revision": "9c67d3837c136ed0b5a5",
+    "url": "/juliaverzosa/Art-Portfolio/static/css/main.f3623059.chunk.css"
+  },
+  {
+    "revision": "dde5cc7536ffd9642ca2",
+    "url": "/juliaverzosa/Art-Portfolio/static/js/2.d7bf948b.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/juliaverzosa/Art-Portfolio/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "9c67d3837c136ed0b5a5",
+    "url": "/juliaverzosa/Art-Portfolio/static/js/main.281d2e30.chunk.js"
+  },
+  {
+    "revision": "b4182467c3b05493f081",
+    "url": "/juliaverzosa/Art-Portfolio/static/js/runtime~main.8a27d661.js"
+  }
+]);
